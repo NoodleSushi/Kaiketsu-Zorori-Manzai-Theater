55edd37041bcf2a1119589fc4097a9cf82f86f7e
NoodleSushi <ytmikesstuffs@gmail.com>
2023-04-02 13:58:39 +0800

    Interpolate Camera in Menu

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -3,4 +3,4 @@ extends Camera2D
 func _process(delta: float) -> void:
 	var viewport_rect : Rect2 = get_viewport_rect()
 	var mouse_pos : Vector2 = get_viewport().get_mouse_position()
-	position = 8 * (mouse_pos - viewport_rect.size / 2) / viewport_rect.size.y
+	position = position.linear_interpolate(32 * (mouse_pos - viewport_rect.size / 2) / viewport_rect.size.y, 1 - pow(.025, delta))
